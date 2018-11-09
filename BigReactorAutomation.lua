66259aeb0316f6b03f9948ebb47f04bebf08adec
Unknown <cancelo.garcia@gmail.com>
2018-11-09 18:03:14 +0100

    Init push

---
+++ BigReactorAutomation.lua
@@ -0,0 +1,25 @@
+Init()
+
+function CheckError()
+    if monitor[1] == nil then
+        error("No monitor attached")
+    end
+
+    if reactor[1] == nil then
+        error("No reactor attached")
+    end
+end
+
+function WriteInMonitor(message)
+    monitor.write(message)
+end
+
+function Init()
+    _G.monitor = peripheral.find("monitor")
+    _G.reactor = {peripheral.find("BigReactors-Reactor")}
+
+    mon.clear();
+end
+
+CheckError();
+WriteInMonitor("Hellow World")
